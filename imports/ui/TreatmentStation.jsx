68e38100969af85d1a0b75efdef33d5b95d1a26d
Richard-zhang <shuhao.zhang15@imperial.ac.uk>
2017-11-18 11:14:52 +0000

    #BestEver improve game scene

--- imports/ui/TreatmentStation.jsx
+++ imports/ui/TreatmentStation.jsx
@@ -12,11 +12,29 @@ export default class App extends Component {
         });
         this.refs.gameCanvas.appendChild(app.view);
 
+        var landscapeTexture = PIXI.Texture.fromImage("/img/beach.jpg");
+
+        // crop the texture to show just 100 px
+        var texture2 = new PIXI.Texture(landscapeTexture, new PIXI.Rectangle(0, 150, 1000, 300));
+
+        // new sprite
+        var background = new PIXI.Sprite(texture2);
+
+        background.anchor.x = 0;
+        background.anchor.y = 0;
+
+        background.position.x = 0;
+        background.position.y = 0;
+
+        app.stage.addChild( background );
+
         var style = new PIXI.TextStyle({
           fontFamily: 'Arvo',
           fontSize: 20,
           wordWrap: true,
-          wordWrapWidth: 950
+          wordWrapWidth: 950,
+          paddingTop: 50,
+          paddingLeft: 80
         });
 
         var style2 = new PIXI.TextStyle({
@@ -24,7 +42,9 @@ export default class App extends Component {
           fontSize: 20,
           fill: ['#FF0000', '#FF0000'],
           wordWrap: true,
-          wordWrapWidth:950
+          wordWrapWidth:950,
+          paddingTop: 100,
+          paddingLeft: 80
         });
 
         var styleHover = new PIXI.TextStyle({
@@ -32,12 +52,16 @@ export default class App extends Component {
           fontSize: 20,
           fontWeight: 'bold',
           wordWrap: true,
-          wordWrapWidth: 950
+          wordWrapWidth: 950,
+          paddingTop: 50,
+          paddingLeft: 80,
+          paddingTop: 100,
+          paddingLeft: 80
         });
         var style2Hover = new PIXI.TextStyle({
           fontFamily: 'Arvo',
           fontSize: 20,
-          fill: ['#FF0000', '#FF0000'],
+          fill: ['#FF8c00', '#FF8c00'],
           fontWeight: 'bold',
           wordWrap: true,
           wordWrapWidth: 950
@@ -51,21 +75,21 @@ export default class App extends Component {
         app.stage.addChild(graphics);
 
 
-        var albert = PIXI.Sprite.fromImage('/img/Hat_man1.png')
+        var albert = PIXI.Sprite.fromImage('/img/Hat_man1Flip.png')
         albert.buttonMode = true;
         albert.interactive = true;
         albert.anchor.set(0.5);
-        albert.x = (app.renderer.width * 2) / 3;
+        albert.x = (app.renderer.width * 3) / 4;
         albert.y = app.renderer.height / 2;
         albert.on('pointerdown', function(){
         });
         app.stage.addChild(albert);
 
-        var user = PIXI.Sprite.fromImage('/img/Hat_man2.png');
+        var user = PIXI.Sprite.fromImage('/img/Hat_man2Flip.png');
         user.buttonMode = true;
         user.interactive = true;
         user.anchor.set(0.5);
-        user.x = (app.renderer.width * 1) / 3;
+        user.x = (app.renderer.width * 9) / 10;
         user.y = app.renderer.height / 2;
         user.on('pointerdown', function(){
         });
@@ -81,7 +105,7 @@ export default class App extends Component {
         var treatmentList = [new PIXI.Text('Surgery', style2), new PIXI.Text('Chemotherapy', style2), new PIXI.Text('Radiotherapy', style2)];
 
         var albertText = new PIXI.Text('Albert: Depending on your situation, you might have to do surgery, radiotherapy, or chemotherapy. What did the doctor tell you?', style);
-        albertText.x = 10;
+        albertText.x = 40;
         albertText.y = 200;
         albertText.buttonMode = true;
         albertText.interactive = true;
@@ -98,7 +122,7 @@ export default class App extends Component {
             if (userInt == 1) { //treatment list
               userText.visible = false;
               for (i = 0; i < 3; i++) {
-                treatmentList[i].x = 10 + i * 300;
+                treatmentList[i].x = 40 + i * 300;
                 treatmentList[i].y = 200;
                 treatmentList[i].buttonMode = true;
                 treatmentList[i].interactive = true;
@@ -117,7 +141,7 @@ export default class App extends Component {
         });
 
         var userText = new PIXI.Text('You: What treatment do I need?', style);
-        userText.x = 10;
+        userText.x = 40;
         userText.y = 200;
         userText.buttonMode = true;
         userText.interactive = true;
