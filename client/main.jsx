59063968780d5598c459641c85bd340d06fd7d81
Christopher Conlan <christopher.conlan15@imperial.ac.uk>
2017-11-18 07:31:15 +0000

    added new cloud sprite#

--- client/main.jsx
+++ client/main.jsx
@@ -6,6 +6,18 @@ import App from '../imports/ui/App.jsx';
 import Map from '../imports/ui/Map.jsx';
 import Cloud from '../imports/ui/Cloud.jsx';
 
+
+// find out coords via image scale
+
+// var img = document.getElementById("board");
+// var height = img.height;
+// var width = img.width;
+
+const coords="0, 0, 100, 102";
+
+
+
 Meteor.startup(() => {
+  document.getElementById("firstRegion").coords = coords;
   render(<Cloud />, document.getElementById('render-target'));
 });
