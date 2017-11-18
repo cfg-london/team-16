3224d14b4a68327c46ee1a407b94dae0b42073d8
Richard-zhang <shuhao.zhang15@imperial.ac.uk>
2017-11-18 07:29:56 +0000

    more clouds

--- imports/ui/Cloud.jsx
+++ imports/ui/Cloud.jsx
@@ -7,7 +7,15 @@ export default class Cloud extends Component {
         super(props);
     }
 
+
+
     componentDidMount() {
+
+        function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+        }
 
         var app = new PIXI.Application(window.innerWidth, window.innerHeight, {
             transparent: true
@@ -17,16 +25,20 @@ export default class Cloud extends Component {
 
         //var sprite = PIXI.Sprite.fromImage('/img/cloud2.png');
 
-        var numClouds = 10;
+        var numClouds = 5;
 
         var sprites = new Array(numClouds);
         for (var i = 0; i < numClouds; i ++) {
           sprites[i] = PIXI.Sprite.fromImage('/img/cloud3.png')
         }
 
+        var spritesRight = new Array(numClouds);
+        for (var i = 0; i < numClouds; i ++) {
+          spritesRight[i] = PIXI.Sprite.fromImage('/img/cloud3.png')
+        }
 
         for (var i = 0; i < numClouds; i ++) {
-          var randy = Math.random()*app.renderer.width;
+          var randy = Math.random()*app.renderer.height;
           var randx = Math.random()*app.renderer.width;
 
           sprites[i].x =  randx;
@@ -34,14 +46,26 @@ export default class Cloud extends Component {
           app.stage.addChild(sprites[i]);
         }
 
+        var diff = 10;
+        for (var i = 0; i < numClouds; i++) {
+            var randy = Math.random()*app.renderer.height;
+            var randx = app.renderer.width - diff - getRandomInt(10, 40);
+            spritesRight[i].x = randx
+            spritesRight[i].y = randy
+            app.stage.addChild(spritesRight[i]);
+            diff += 10;
+        }
 
 
         var xVelocity = 1;
+        var yVelocity = 1;
 
         var goingLeft = new Array(numClouds);
+        var goingRight = new Array(numClouds);
 
         for (var i = 0; i < numClouds; i ++) {
           goingLeft[i] = true;
+          goingRight[i] = true;
         }
 
         //var goingLeft = true;
@@ -49,12 +73,27 @@ export default class Cloud extends Component {
         var leftBound = 100;
         var RightBound = app.renderer.width / 2;
 
+        var secondLeftBound = app.renderer.width * 0.65;
+        var secondRightBound = app.renderer.width - 100;
+
         app.ticker.add(()=> {
           for (var j = 0; j < numClouds; j ++) {
             if (goingLeft[j]) {
                 sprites[j].x += xVelocity;
             } else {
                 sprites[j].x -= xVelocity;
+            }
+
+            if (goingRight[j]) {
+                spritesRight[j].x -= yVelocity;
+            } else {
+                spritesRight[j].x += yVelocity;
+            }
+
+            if (spritesRight[j].x < secondLeftBound) {
+                goingRight[j] = false;
+            } else if (spritesRight[j].x > secondRightBound){
+                goingRight[j] = true;
             }
 
             if (sprites[j].x > RightBound) {
